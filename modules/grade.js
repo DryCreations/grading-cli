eb6a57854e3d05411f8b9f23260235e5be717a23
DryCreations <DryCreations@gmail.com>
2021-10-26 22:28:11 -0400

    Mass run test cases on assignments Fixes #5 #6

--- modules/grade.js
+++ modules/grade.js
@@ -1,6 +1,7 @@
 
 import { Select } from "https://deno.land/x/cliffy/prompt/select.ts";
 import { Input } from "https://deno.land/x/cliffy/prompt/input.ts";
+import { Table } from "https://deno.land/x/cliffy/table/mod.ts";
 import { get_repos } from "./git.js";
 
 let cache = {};
@@ -35,14 +36,14 @@ export default async function(octokit) {
                     grader = await get_grader_repo(octokit);
                 }
             },
-            'repos': {
-                disabled: false,
-                name: `Select repos ${repos ? '*SET*' : ''}`,
-                action: async () => {
-                    csv = undefined;
-                    repos = [];
-                }
-            },
+            // 'repos': {
+            //     disabled: false,
+            //     name: `Select repos ${repos ? '*SET*' : ''}`,
+            //     action: async () => {
+            //         csv = undefined;
+            //         repos = [];
+            //     }
+            // },
             'start': {
                 disabled: (!repos || !grader),
                 name: 'Start Grading',
@@ -68,25 +69,6 @@ export default async function(octokit) {
     
         await options[choice].action();
     } while (!exit_flag)
-
-    /*
-
-    Select CSV (Hold State) (Optional, will set select repos)
-        Pick file from data folder
-    Select Grader (Hold State) (Required)
-        Select grader repo, then clone it locally
-    Select Repos (Hold State) (Required, will unset CSV)
-        Select repos from downloaded
-    Start Grading (Disabled if (!Repos || !Grader))
-        Runs tests
-        combines data with grading CSV, if present
-        print tabulated data of all students
-        options:
-            Export CSV
-            Export Feedback
-            Push feedback to student repos in issue
-
-    */
 }
 
 async function selectCSV(dir) {
@@ -211,5 +193,135 @@ async function get_grader_repo(octokit) {
 }
 
 async function start_grading(octokit, csv, repos, grader) {
-    console.log("start grading");
+    let grading_info = JSON.parse(await Deno.readTextFile(`${grader}/grading.json`));
+    let test_cases = grading_info.tests;
+    const req = { name: "run" };
+    const perm = await Deno.permissions.request(req);
+    const {token} = await octokit.auth({ type: "get" });
+    if (perm.state == "granted") { 
+        let dir = `${grader}/${grading_info.grader}`;
+        for (let s of grading_info.setup) {    
+            const c = Deno.run({
+                cwd: dir,
+                cmd: ["bash", "-c", s]
+            })
+    
+            await c.status();
+        }
+        let grader_results = await Promise.all(test_cases.map(test => {
+            return run_test(dir, test);
+        }));
+        
+        let dirs = await Promise.all(repos.map(async repo => {
+            let reg = /(?<=https:\/\/github\.com\/).*/;
+            let [repo_name] = repo.match(reg);
+            let dir_name = `./tmp/repos/${repo_name}`;
+           
+            const rm = Deno.run({
+                cmd: ["rm", "-rf", dir_name]
+            })
+
+            await rm.status();
+            
+            const clone = Deno.run({
+                cmd: ["git", "clone", repo.replace('https://', 'https://' + token + '@'), dir_name]
+            })
+            
+            await clone.status();
+
+            return dir_name;
+        }))
+
+        let test_results = await Promise.all(dirs.map(d => {
+            return run_tests(d, grading_info);
+        }));
+
+        let scores = await Promise.all(test_results.map(result => score_tests(result, grader_results, grading_info)));
+
+        let max = grading_info.tests.reduce((prev, curr)=>{return curr.points + prev},0);
+
+        let headers = ["repo_url", "score", "max"];
+
+        let rows = [];
+
+        repos.forEach((repo, idx) => {
+            rows.push([repo, scores[idx].reduce((a,b) => a + b, 0), max])
+        });
+
+        if (csv) {
+            rows.forEach(row => {
+                let e = csv.find(el => row[0] == el.student_repository_url);
+                row.unshift(e.roster_identifier)
+                row.push(e.points_awarded)
+            })
+            headers.unshift('name');
+            headers.push('autograder')
+        }
+
+        new Table()
+            .header(headers)
+            .body(rows)
+            .render();
+
+        
+
+    } else {
+        console.log('failed to grade assignment -- please allow run permissions')
+    }
+}
+
+async function score_tests(student, grader, grading_info) {
+    return Promise.all(grading_info.tests.map((test, idx) => {
+        let score;
+        switch (test.comparison) {
+            case "lev":
+                score = 0;
+                break;
+            case "exact":
+            default:
+                score = student[idx].stdout == grader[idx].stdout ? 1 : 0;
+                break;
+        }
+
+        return score * test.points;
+    }));
+}
+
+async function run_tests(dir, grading_info) {
+    for (let s of grading_info.setup) {    
+        const c = Deno.run({
+            cwd: dir,
+            cmd: ["bash", "-c", s]
+        })
+
+        await c.status();
+    }
+
+    return Promise.all(grading_info.tests.map(test => {
+        return run_test(dir, test);
+    }));
+
+}
+
+async function run_test(dir, test) {
+    const r = Deno.run({
+        cwd: dir,
+        cmd: ["bash", "-c", test.run],
+        stdout: 'piped',
+        stdin: 'piped',
+        stderr: 'piped'
+    })
+
+    await r.stdin.write(new TextEncoder().encode(test.input));
+    await r.stdin.close();
+
+    await r.status();
+
+    let decoder = new TextDecoder();
+
+    return {
+        stdin: test.input,
+        stdout: decoder.decode(await r.output()),
+        stderr: decoder.decode(await r.stderrOutput())
+    }
 }
